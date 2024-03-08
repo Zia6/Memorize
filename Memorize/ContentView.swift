d6d36bc0d33b62c4feb64b9aed7319e05a3d8c19
zxx <z20230404@gmail.com>
2024-03-08 14:08:39 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 原神 on 2024/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView(isFaceUP: true);
+            CardView();
+            CardView();
+            CardView();
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+struct CardView: View{
+    var isFaceUP: Bool = false
+    var body: some View{
+        ZStack {
+            if isFaceUP {
+                RoundedRectangle(cornerRadius:  12)
+                    .strokeBorder(lineWidth: 2)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(.white)
+                Text("😂").font(.largeTitle)
+            }else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 原神 on 2024/3/6.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
